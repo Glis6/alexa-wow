9afd27ebb156d39f0cb44c0c75e9a4ca384e75e1
Michael Myers <michael.morris.myers@gmail.com>
2017-02-08 21:44:08 -0500

    typo

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
 import * as Alexa from "alexa-sdk";
 
 import HelloWorldController from "./controllers/HelloWorldController";
-import WordlyHelloController from "./controllers/WorldlyHelloController";
+import WorldlyHelloController from "./controllers/WorldlyHelloController";
 
 let handler = function(event: Alexa.RequestBody, context: Alexa.Context, callback: Function): void {
     let alexa = Alexa.handler(event, context, callback);
     alexa.registerHandlers({
         "LaunchRequest": function() { new HelloWorldController(this).sayHello(); },
         "HelloWorldIntent": function() { new HelloWorldController(this).sayHello(); },
-        "WorldlyHelloIntent": function() { new WordlyHelloController(this).sayHello(); }
+        "WorldlyHelloIntent": function() { new WorldlyHelloController(this).sayHello(); }
     });
     alexa.execute();
 };
