f982b0f843145b797686ba71a4cfb6fc4100dd77
coreycole <coreyleoc@gmail.com>
2018-04-22 22:11:58 -0700

    tests(worldly-hello-intent): fix bady copy paste in tests

--- src/services/WorldlyHello.test.ts
+++ src/services/WorldlyHello.test.ts
@@ -11,19 +11,19 @@ describe("WorldlyHello", function() {
             });
         });
         it("returns the correct response for a known language (welsh)", function() {
-            return WorldlyHello.getHello({language: "english"}).then((response) => {
+            return WorldlyHello.getHello({language: "welsh"}).then((response) => {
                 expect(response.text).to.equal("Helo");
                 expect(response.ssml).to.equal("<speak>Helo</speak>");
             });
         });
         it("returns the correct response for a known language (latin)", function() {
-            return WorldlyHello.getHello({language: "english"}).then((response) => {
+            return WorldlyHello.getHello({language: "latin"}).then((response) => {
                 expect(response.text).to.equal("Salve");
                 expect(response.ssml).to.equal("<speak>Salve</speak>");
             });
         });
         it("returns the correct response for a known language (esperanto)", function() {
-            return WorldlyHello.getHello({language: "english"}).then((response) => {
+            return WorldlyHello.getHello({language: "esperanto"}).then((response) => {
                 expect(response.text).to.equal("Saluton");
                 expect(response.ssml).to.equal("<speak>Saluton</speak>");
             });
