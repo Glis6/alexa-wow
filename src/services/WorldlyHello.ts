29024585bce10922f192aaf6f3311ca2a8bedae9
coreycole <coreyleoc@gmail.com>
2018-04-22 18:46:45 -0700

    feat(worldy-hello-service): try printing entire request obejct for error

--- src/services/WorldlyHello.ts
+++ src/services/WorldlyHello.ts
@@ -26,7 +26,7 @@ namespace WorldlyHello {
                     ssml: response.ssml
                 });
             } else {
-                reject(new Error(`Unknown language: ${language}`));
+                reject(new Error(`Unknown language, request=${request}`));
             }
         });
     }
